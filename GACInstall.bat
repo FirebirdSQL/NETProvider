7849656c1f7c76f5d5fb014ca102f190b6c1ee73
carlosga_fb <carlosga05@gmail.com>
2006-12-25 10:30:00 +0000

    cleanup

--- GACInstall.bat
+++
@@ -1,3 +0,0 @@
-"C:\Archivos de programa\Microsoft Visual Studio 8\SDK\v2.0\Bin\gacutil.exe" -u FirebirdSql.Data.FirebirdClient
-cd .\source\FirebirdSql\bin\Debug
-"C:\Archivos de programa\Microsoft Visual Studio 8\SDK\v2.0\Bin\gacutil.exe" -i FirebirdSql.Data.FirebirdClient.dll
