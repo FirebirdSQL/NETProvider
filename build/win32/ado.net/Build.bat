0498c0259b08f785b0f7ad349de0361bbd7e05a4
cincura_net <jiri@x2develop.com>
2008-01-08 10:58:43 +0000

    Batch file restructured to we be able to build without any manual changes before. Functionality is same.

--- build/win32/ado.net/Build.bat
+++ build/win32/ado.net/Build.bat
@@ -1,1 +1,9 @@
-d:\desarrollo\herramientas\nant\bin\nant -verbose > log.txt
+@echo off
+
+IF "%USERNAME%" == "Jiri" ( rem Jiri
+  SET NANT=I:\Downloads\nant-0.85\bin\NAnt.exe
+) ELSE ( rem Carlos
+  SET NANT=d:\desarrollo\herramientas\nant\bin\nant
+)
+
+%NANT% -verbose > Build.log
