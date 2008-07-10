1644c8bfbd9c3f7e9d75c5ec0f853f7e0d3c3fa2
cincura_net <jiri@x2develop.com>
2008-07-10 09:09:22 +0000

    Changed String to string.

--- NETProvider/source/FirebirdSql/Data/FirebirdClient/FbDataReader.cs
+++ NETProvider/source/FirebirdSql/Data/FirebirdClient/FbDataReader.cs
@@ -59,7 +59,7 @@ namespace FirebirdSql.Data.FirebirdClient
             get { return this.GetValue(i); }
         }
 
-        public override object this[String name]
+        public override object this[string name]
         {
             get { return this.GetValue(this.GetOrdinal(name)); }
         }
