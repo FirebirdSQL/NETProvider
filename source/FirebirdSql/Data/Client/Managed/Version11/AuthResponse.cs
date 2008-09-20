e8c6ca6b1da829e76cd0d66b440b11bad38c802f
cincura_net <jiri@x2develop.com>
2008-09-20 14:31:44 +0000

    Added standard header.

--- source/FirebirdSql/Data/Client/Managed/Version11/AuthResponse.cs
+++ source/FirebirdSql/Data/Client/Managed/Version11/AuthResponse.cs
@@ -1,3 +1,22 @@
+/*
+ *  Firebird ADO.NET Data provider for .NET and Mono 
+ * 
+ *     The contents of this file are subject to the Initial 
+ *     Developer's Public License Version 1.0 (the "License"); 
+ *     you may not use this file except in compliance with the 
+ *     License. You may obtain a copy of the License at 
+ *     http://www.firebirdsql.org/index.php?op=doc&id=idpl
+ *
+ *     Software distributed under the License is distributed on 
+ *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
+ *     express or implied.  See the License for the specific 
+ *     language governing rights and limitations under the License.
+ * 
+ *  Copyright (c) 2008 Vladimir Bodecek, Jiri Cincura (jiri@cincura.net)
+ *  All Rights Reserved.
+ *  
+ */
+
 #if (TRUSTED_AUTH)
 
 using System;
@@ -5,7 +24,7 @@ using FirebirdSql.Data.Client.Managed;
 
 namespace FirebirdSql.Data.Client.Managed.Version11
 {
-    class AuthResponse : IResponse
+    internal class AuthResponse : IResponse
     {
         #region · Fields ·
 
