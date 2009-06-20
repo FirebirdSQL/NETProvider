b9759c2a5e3eab5629a61a5854bdf83aa0b18e29
cincura_net <jiri@x2develop.com>
2009-06-20 17:02:05 +0000

    Proper part of version into model (GetDbProviderManifestToken).

--- NETProvider/source/FirebirdSql/Data/FirebirdClient/FbProviderServices.cs
+++ NETProvider/source/FirebirdSql/Data/FirebirdClient/FbProviderServices.cs
@@ -199,7 +199,7 @@ namespace FirebirdSql.Data.FirebirdClient
                     closeConnection = true;
                 }
 
-                return fbConnection.ServerVersionNumber.ToString(4);
+                return fbConnection.ServerVersionNumber.ToString(2);
             }
             finally
             {
