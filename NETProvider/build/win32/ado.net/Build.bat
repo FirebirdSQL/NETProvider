26c8593b387b88de27b3933aebb68277790181b0
cincura_net <jiri@x2develop.com>
2009-09-12 20:25:21 +0000

    Removing the obsolete scripts (check the "RFC: Building FirebirdClient" thread in list).

--- NETProvider/build/win32/ado.net/Build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-IF "%USERNAME%" == "Jiri" ( rem Jiri
-  SET NANT=I:\Downloads\nant-0.85\bin\NAnt.exe
-) ELSE ( rem Carlos
-  SET NANT=d:\desarrollo\herramientas\nant\bin\nant
-)
-
-%NANT% -verbose > Build.log
