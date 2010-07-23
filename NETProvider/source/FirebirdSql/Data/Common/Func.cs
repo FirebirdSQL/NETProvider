0f9851191f22d80620e3a25f40b27c9eb58ff647
cincura_net <jiri@x2develop.com>
2010-07-23 12:57:05 +0000

    Fixed argument.

--- NETProvider/source/FirebirdSql/Data/Common/Func.cs
+++ NETProvider/source/FirebirdSql/Data/Common/Func.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace FirebirdSql.Data.Common
 {
 #if (NET_20)
 		delegate TResult Func<TResult>();
-		delegate TResult Func<T, TResult>();
+		delegate TResult Func<T, TResult>(T arg);
 #endif
 }
