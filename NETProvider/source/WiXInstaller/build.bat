ff2607c00b361f694e50dc7ae1c3001bc0891ff8
cincura_net <jiri@x2develop.com>
2010-10-25 13:55:11 +0000

    Direct path to WiX binaries.

--- NETProvider/source/WiXInstaller/build.bat
+++ NETProvider/source/WiXInstaller/build.bat
@@ -1,5 +1,5 @@
-set WIX=C:\Program Files\Windows Installer XML v3.0\
+set WIX=C:\Program Files\Windows Installer XML v3.0\bin\
 set SOLUTION_DIR=%~dp0%..\
 
-"%WIX%bin\candle.exe" -dSolutionDir=%SOLUTION_DIR% -out "%SOLUTION_DIR%WixInstaller\out\Install.wixobj" "%SOLUTION_DIR%WixInstaller\Install.wxs"
-"%WIX%bin\light.exe" -ext "%WIX%bin\WixUIExtension.dll" -out "%SOLUTION_DIR%WixInstaller\out\NETProvider.msi" "%SOLUTION_DIR%WixInstaller\out\Install.wixobj"
+"%WIX%candle.exe" -dSolutionDir=%SOLUTION_DIR% -out "%SOLUTION_DIR%WiXInstaller\out\Install.wixobj" "%SOLUTION_DIR%WiXInstaller\Install.wxs"
+"%WIX%light.exe" -ext "%WIX%WixUIExtension.dll" -out "%SOLUTION_DIR%WiXInstaller\out\NETProvider.msi" "%SOLUTION_DIR%WiXInstaller\out\Install.wixobj"
