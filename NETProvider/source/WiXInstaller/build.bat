afc2b773ec5cc9ec18e16864508e80224a27f0c9
cincura_net <jiri@x2develop.com>
2010-10-25 12:22:39 +0000

    DNET-350   * WiX cleanup.   * Using WiX directly.   * Using more stuff from WiX toolset.

---
+++ NETProvider/source/WiXInstaller/build.bat
@@ -0,0 +1,5 @@
+set WIX=C:\Program Files\Windows Installer XML v3.5\
+set SOLUTION_DIR=%~dp0%..\
+
+"%WIX%bin\candle.exe" -dSolutionDir=%SOLUTION_DIR% -out "%SOLUTION_DIR%WixInstaller\out\Install.wixobj" "%SOLUTION_DIR%WixInstaller\Install.wxs"
+"%WIX%bin\light.exe" -ext "%WIX%bin\WixUIExtension.dll" -out "%SOLUTION_DIR%WixInstaller\out\NETProvider.msi" "%SOLUTION_DIR%WixInstaller\out\Install.wixobj"
