9eb016b3be5a88b51d9702789feaa44e319ef901
cincura_net <jiri@x2develop.com>
2012-07-26 09:57:11 +0000

    * Signing UnitTests. * Added InternalsVisibleTo. * Added FbConnectionStringTests.

--- NETProvider/source/FirebirdSql/Data/Properties/AssemblyInfo.cs
+++ NETProvider/source/FirebirdSql/Data/Properties/AssemblyInfo.cs
@@ -14,6 +14,9 @@
  * 
  *  Copyright (c) 2002, 2007 Carlos Guzman Alvarez
  *  All Rights Reserved.
+ *	
+ *  Contributors:
+ *      Jiri Cincura (jiri@cincura.net)
  */
 
 using System;
@@ -32,3 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("(c) 2002-2012")]
 [assembly: AssemblyVersion("2.7.7")]
 [assembly: AssemblyDelaySign(false)]
+[assembly: InternalsVisibleTo("FirebirdSql.Data.UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002f636382c6d70ed5596f3db517cf3bf37950ee9ee86340d32d6f98143f0a4fdf0e934d361de0a6ce63c61e0a0dddc5f66d8ec752306b94931241061817f3c203e1105da8958ca9a889af83083bbb53dfdfee2d028d554bef2ce8a577816202a7bb38885e2dc74695d2a0fecfef259a34860a8faf54ce49a0cd5b5fdfa90f4bb7")]
---
+++ NETProvider/source/FirebirdSql/Data/UnitTests/FbConnectionStringTests.cs
@@ -0,0 +1,71 @@
+﻿/*
+ *  Firebird ADO.NET Data provider for .NET and Mono 
+ * 
+ *     The contents of this file are subject to the Initial 
+ *     Developer's Public License Version 1.0 (the "License"); 
+ *     you may not use this file except in compliance with the 
+ *     License. You may obtain a copy of the License at 
+ *     http://www.firebirdsql.org/index.php?op=doc&id=idpl
+ *
+ *     Software distributed under the License is distributed on 
+ *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
+ *     express or implied.  See the License for the specific 
+ *     language governing rights and limitations under the License.
+ * 
+ *  Copyright (c) 2012 Jiri Cincura (jiri@cincura.net)
+ *  All Rights Reserved.
+ */
+
+using System;
+using System.Configuration;
+using System.Data;
+using System.Reflection;
+
+using FirebirdSql.Data.FirebirdClient;
+using NUnit.Framework;
+
+namespace FirebirdSql.Data.UnitTests
+{
+	[TestFixture]
+	public class FbConnectionStringTests
+	{
+		[Test]
+		public void ParsingNormalConnectionStringTest()
+		{
+			const string ConnectionString = "datasource=testserver;database=testdb.fdb;user=testuser;password=testpwd";
+			var cs = new FbConnectionString(ConnectionString);
+			Assert.AreEqual("testserver", cs.DataSource);
+			Assert.AreEqual("testdb.fdb", cs.Database);
+			Assert.AreEqual("testuser", cs.UserID);
+			Assert.AreEqual("testpwd", cs.Password);
+		}
+
+		[Test]
+		public void ParsingFullDatabaseConnectionStringTest()
+		{
+			const string ConnectionString = "database=testserver/1234:testdb.fdb;user=testuser;password=testpwd";
+			var cs = new FbConnectionString(ConnectionString);
+			Assert.AreEqual("testserver", cs.DataSource);
+			Assert.AreEqual("testdb.fdb", cs.Database);
+			Assert.AreEqual("testuser", cs.UserID);
+			Assert.AreEqual("testpwd", cs.Password);
+			Assert.AreEqual(1234, cs.Port);
+		}
+
+		[Test]
+		public void ParsingSingleQuotedConnectionStringTest()
+		{
+			const string ConnectionString = "datasource=testserver;database=testdb.fdb;user=testuser;password=test'pwd";
+			var cs = new FbConnectionString(ConnectionString);
+			Assert.AreEqual("test'pwd", cs.Password);
+		}
+
+		[Test]
+		public void ParsingDoubleQuotedConnectionStringTest()
+		{
+			const string ConnectionString = "datasource=testserver;database=testdb.fdb;user=testuser;password=test\"pwd";
+			var cs = new FbConnectionString(ConnectionString);
+			Assert.AreEqual("test\"pwd", cs.Password);
+		}
+	}
+}
