a7c9c656e612e621e350986472343b1029f16443
cincura_net <jiri@x2develop.com>
2012-10-01 11:06:27 +0000

    Changed paths.

--- NETProvider/installer/build.bat
+++ NETProvider/installer/build.bat
@@ -1,5 +1,5 @@
 set WIX=C:\Program Files\Windows Installer XML v3.0\bin\
 set SOLUTION_DIR=%~dp0%..\
 
-"%WIX%candle.exe" -dSolutionDir=%SOLUTION_DIR% -out "%SOLUTION_DIR%WiXInstaller\out\Install.wixobj" "%SOLUTION_DIR%WiXInstaller\Install.wxs"
-"%WIX%light.exe" -ext "%WIX%WixUIExtension.dll" -out "%SOLUTION_DIR%WiXInstaller\out\NETProvider.msi" "%SOLUTION_DIR%WiXInstaller\out\Install.wixobj"
+"%WIX%candle.exe" -dSolutionDir=%SOLUTION_DIR% -out "%SOLUTION_DIR%installer\out\Install.wixobj" "%SOLUTION_DIR%installer\Install.wxs"
+"%WIX%light.exe" -ext "%WIX%WixUIExtension.dll" -out "%SOLUTION_DIR%installer\out\NETProvider.msi" "%SOLUTION_DIR%installer\out\Install.wixobj"
