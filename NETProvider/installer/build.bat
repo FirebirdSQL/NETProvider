5ccf4db737d1594d6e85a5471da7dfc0efdfe259
cincura_net <jiri@x2develop.com>
2012-10-02 06:05:16 +0000

    Renamed build file and switched to PS.

--- NETProvider/installer/build.bat
+++
@@ -1,5 +0,0 @@
-set WIX=I:\devel\bin\wix35-binaries\
-set BASE_DIR=%~dp0%..\
-
-"%WIX%candle.exe" -dBaseDir=%BASE_DIR% -out "%BASE_DIR%installer\out\Install.wixobj" "%BASE_DIR%installer\Install.wxs"
-"%WIX%light.exe" -ext "%WIX%WixUIExtension.dll" -out "%BASE_DIR%installer\out\NETProvider.msi" "%BASE_DIR%installer\out\Install.wixobj"
