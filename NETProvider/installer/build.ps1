de61825e560e221ab2a85971e891f441b9baa13b
cincura_net <jiri@x2develop.com>
2012-10-02 08:11:17 +0000

    Added reference to WixUtilExtension.

--- NETProvider/installer/build.ps1
+++ NETProvider/installer/build.ps1
@@ -1,6 +1,6 @@
 $wix = 'I:\devel\bin\wix-binaries'
 $baseDir = Split-Path -Parent (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-& $wix\candle.exe "-dBaseDir=$baseDir" -out $baseDir\installer\out\Install.wixobj $baseDir\installer\Install.wxs
+& $wix\candle.exe "-dBaseDir=$baseDir" -ext $wix\WixUtilExtension.dll -out $baseDir\installer\out\Install.wixobj $baseDir\installer\Install.wxs
 & $wix\light.exe -ext $wix\WixUIExtension.dll -ext $wix\WixUtilExtension.dll -out $baseDir\installer\out\NETProvider.msi $baseDir\installer\out\Install.wixobj
 
