7a3b1a0a87c19f5d3d1d36216d10a900d61cf5e1
Jiri Cincura <jiri@x2develop.com>
2013-07-27 18:11:25 +0200

    Added proper overload.

--- NETProvider/source/FirebirdSql/Data/Common/TraceHelper.cs
+++ NETProvider/source/FirebirdSql/Data/Common/TraceHelper.cs
@@ -15,6 +15,12 @@ namespace FirebirdSql.Data.Common
 			_instance = new TraceSource(Name, SourceLevels.All);
 		}
 
+		public static void Trace(TraceEventType eventType, string message)
+		{
+			_instance.TraceEvent(eventType, default(int), message);
+			_instance.Flush();
+		}
+
 		public static void Trace(TraceEventType eventType, string format, params object[] args)
 		{
 			_instance.TraceEvent(eventType, default(int), format, args);
