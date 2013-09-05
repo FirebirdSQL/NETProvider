8a443d77f95f0ff17c1b7ef6c2aae31f96ba91eb
Jiri Cincura <jiri@x2develop.com>
2013-09-05 09:28:24 +0200

    Added FbNBackup (DNET-224).

---
+++ NETProvider/source/FirebirdSql/Data/Services/FbNBackup.cs
@@ -0,0 +1,89 @@
+﻿/*
+ *	Firebird ADO.NET Data provider for .NET and Mono 
+ * 
+ *	   The contents of this file are subject to the Initial 
+ *	   Developer's Public License Version 1.0 (the "License"); 
+ *	   you may not use this file except in compliance with the 
+ *	   License. You may obtain a copy of the License at 
+ *	   http://www.firebirdsql.org/index.php?op=doc&id=idpl
+ *
+ *	   Software distributed under the License is distributed on 
+ *	   an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
+ *	   express or implied. See the License for the specific 
+ *	   language governing rights and limitations under the License.
+ * 
+ *	Copyright (c) 2013 Jiri Cincura (jiri@cincura.net)
+ *	All Rights Reserved.
+ */
+
+using System;
+
+using FirebirdSql.Data.Common;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace FirebirdSql.Data.Services
+{
+	public sealed class FbNBackup : FbService
+	{
+		#region Properties
+		int level;
+		public int Level
+		{
+			get { return this.level; }
+			set
+			{
+				if (value < 0)
+					throw new ArgumentOutOfRangeException();
+				this.level = value;
+			}
+		}
+		public string BackupFile { get; set; }
+		public bool DirectIO { get; set; }
+		public FbNBackupFlags Options { get; set; }
+		#endregion
+
+		#region Constructors
+		public FbNBackup()
+			: base()
+		{
+		}
+		#endregion
+
+		#region Methods
+		public void Execute()
+		{
+			try
+			{
+				// Configure Spb
+				this.StartSpb = new ServiceParameterBuffer();
+
+				this.StartSpb.Append(IscCodes.isc_action_svc_nbak);
+				this.StartSpb.Append(IscCodes.isc_spb_dbname, this.Database);
+
+				this.StartSpb.Append(IscCodes.isc_spb_nbk_level, this.level);
+				this.StartSpb.Append(IscCodes.isc_spb_nbk_file, this.BackupFile);
+
+				this.StartSpb.Append(IscCodes.isc_spb_nbk_direct, this.DirectIO ? "ON" : "OFF");
+
+				this.StartSpb.Append(IscCodes.isc_spb_options, (int)this.Options);
+
+				this.Open();
+
+				// Start execution
+				this.StartTask();
+
+				this.ProcessServiceOutput();
+			}
+			catch (Exception ex)
+			{
+				throw new FbException(ex.Message, ex);
+			}
+			finally
+			{
+				// Close
+				this.Close();
+			}
+		}
+		#endregion
+	}
+}
