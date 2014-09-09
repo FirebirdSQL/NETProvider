71074b8c40b2ecb0b1b28bcdff8a779faa2c9567
Jiri Cincura <jiri@x2develop.com>
2014-09-09 17:27:46 +0200

    Bumped version.

--- NETProvider/src/FirebirdSql.Data.FirebirdClient/Properties/VersionInfo.cs
+++ NETProvider/src/FirebirdSql.Data.FirebirdClient/Properties/VersionInfo.cs
@@ -24,6 +24,6 @@ namespace FirebirdSql.Data.FirebirdClient.Properties
 {
 	static class VersionInfo
 	{
-		internal const string Version = "4.5.0.0";
+		internal const string Version = "4.5.1.0";
 	}
 }
