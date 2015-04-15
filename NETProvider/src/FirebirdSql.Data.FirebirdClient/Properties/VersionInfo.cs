daf7eb7d50c7efdc8ef05c336bacba8c3d523ac8
Jiri Cincura <jiri@x2develop.com>
2015-04-15 15:35:08 +0200

    New version.

--- NETProvider/src/FirebirdSql.Data.FirebirdClient/Properties/VersionInfo.cs
+++ NETProvider/src/FirebirdSql.Data.FirebirdClient/Properties/VersionInfo.cs
@@ -24,6 +24,6 @@ namespace FirebirdSql.Data.FirebirdClient.Properties
 {
 	static class VersionInfo
 	{
-		internal const string Version = "4.6.1.0";
+		internal const string Version = "4.6.2.0";
 	}
 }
