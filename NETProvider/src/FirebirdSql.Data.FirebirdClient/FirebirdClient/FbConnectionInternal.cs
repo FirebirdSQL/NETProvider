d4b1ed08f6de86911a6a36022400e4d13f1b72c3
Jiri Cincura <jiri@x2develop.com>
2015-07-03 10:20:06 +0200

    Proped method name.

--- NETProvider/src/FirebirdSql.Data.FirebirdClient/FirebirdClient/FbConnectionInternal.cs
+++ NETProvider/src/FirebirdSql.Data.FirebirdClient/FirebirdClient/FbConnectionInternal.cs
@@ -1,20 +1,20 @@
 /*
- *	Firebird ADO.NET Data provider for .NET and Mono 
- * 
- *	   The contents of this file are subject to the Initial 
- *	   Developer's Public License Version 1.0 (the "License"); 
- *	   you may not use this file except in compliance with the 
- *	   License. You may obtain a copy of the License at 
+ *	Firebird ADO.NET Data provider for .NET and Mono
+ *
+ *	   The contents of this file are subject to the Initial
+ *	   Developer's Public License Version 1.0 (the "License");
+ *	   you may not use this file except in compliance with the
+ *	   License. You may obtain a copy of the License at
  *	   http://www.firebirdsql.org/index.php?op=doc&id=idpl
  *
- *	   Software distributed under the License is distributed on 
- *	   an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
- *	   express or implied. See the License for the specific 
+ *	   Software distributed under the License is distributed on
+ *	   an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *	   express or implied. See the License for the specific
  *	   language governing rights and limitations under the License.
- * 
+ *
  *	Copyright (c) 2002, 2007 Carlos Guzman Alvarez
  *	All Rights Reserved.
- * 
+ *
  *  Contributors:
  *      Jiri Cincura (jiri@cincura.net)
  */
@@ -120,7 +120,7 @@ namespace FirebirdSql.Data.FirebirdClient
 
 			// This object will be cleaned up by the Dispose method.
 			// Therefore, you should call GC.SupressFinalize to
-			// take this object off the finalization queue 
+			// take this object off the finalization queue
 			// and prevent finalization code for this object
 			// from executing a second time.
 			GC.SuppressFinalize(this);
@@ -429,8 +429,8 @@ namespace FirebirdSql.Data.FirebirdClient
 				}
 				catch (System.IO.IOException)
 				{
-					// If an IO error occurs weh trying to release the command 
-					// avoid it. ( It maybe the connection to the server was down 
+					// If an IO error occurs weh trying to release the command
+					// avoid it. ( It maybe the connection to the server was down
 					// for unknown reasons. )
 				}
 				catch (IscException ex)
@@ -516,7 +516,7 @@ namespace FirebirdSql.Data.FirebirdClient
 				dpb.Append(IscCodes.isc_dpb_user_name, options.UserID);
 				dpb.Append(IscCodes.isc_dpb_password, options.Password);
 			}
-			dpb.Append(IscCodes.isc_dpb_process_id, GetProcessID());
+			dpb.Append(IscCodes.isc_dpb_process_id, GetProcessId());
 			dpb.Append(IscCodes.isc_dpb_process_name, GetProcessName());
 			if (options.NoDatabaseTriggers)
 			{
@@ -574,7 +574,7 @@ namespace FirebirdSql.Data.FirebirdClient
 			}
 		}
 
-		private int GetProcessID()
+		private int GetProcessId()
 		{
 			System.Reflection.Assembly assembly = System.Reflection.Assembly.GetEntryAssembly();
 			if (assembly != null)
