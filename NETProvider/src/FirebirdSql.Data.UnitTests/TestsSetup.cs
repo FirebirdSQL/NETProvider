e1ef3beefe8d7632512208bdb7853441d191fdc5
Jiri Cincura <jiri@x2develop.com>
2015-08-19 08:14:00 +0200

    Good idea to also insert data, right? :)

--- NETProvider/src/FirebirdSql.Data.UnitTests/TestsSetup.cs
+++ NETProvider/src/FirebirdSql.Data.UnitTests/TestsSetup.cs
@@ -68,7 +68,6 @@ namespace FirebirdSql.Data.UnitTests
 			string cs = TestsBase.BuildConnectionString(server);
 			CreateDatabase(cs);
 			CreateTables(cs);
-			InsertTestData(cs);
 			CreateProcedures(cs);
 			CreateTriggers(cs);
 		}
@@ -292,73 +291,6 @@ namespace FirebirdSql.Data.UnitTests
 			command.Dispose();
 
 			connection.Close();
-		}
-
-		private static void InsertTestData(string connectionString)
-		{
-			FbConnection connection = new FbConnection(connectionString);
-			connection.Open();
-
-			StringBuilder commandText = new StringBuilder();
-
-			commandText.Append("insert into	test (int_field, char_field, varchar_field,	bigint_field, smallint_field, float_field, double_field, numeric_field,	date_field,	time_field,	timestamp_field, clob_field, blob_field)");
-			commandText.Append(" values(@int_field,	@char_field, @varchar_field, @bigint_field,	@smallint_field, @float_field, @double_field, @numeric_field, @date_field, @time_field,	@timestamp_field, @clob_field, @blob_field)");
-
-			FbTransaction transaction = connection.BeginTransaction();
-			FbCommand command = new FbCommand(commandText.ToString(), connection, transaction);
-
-			try
-			{
-				// Add command parameters
-				command.Parameters.Add("@int_field", FbDbType.Integer);
-				command.Parameters.Add("@char_field", FbDbType.Char);
-				command.Parameters.Add("@varchar_field", FbDbType.VarChar);
-				command.Parameters.Add("@bigint_field", FbDbType.BigInt);
-				command.Parameters.Add("@smallint_field", FbDbType.SmallInt);
-				command.Parameters.Add("@float_field", FbDbType.Double);
-				command.Parameters.Add("@double_field", FbDbType.Double);
-				command.Parameters.Add("@numeric_field", FbDbType.Numeric);
-				command.Parameters.Add("@date_field", FbDbType.Date);
-				command.Parameters.Add("@time_Field", FbDbType.Time);
-				command.Parameters.Add("@timestamp_field", FbDbType.TimeStamp);
-				command.Parameters.Add("@clob_field", FbDbType.Text);
-				command.Parameters.Add("@blob_field", FbDbType.Binary);
-
-				command.Prepare();
-
-				for (int i = 0; i < 100; i++)
-				{
-					command.Parameters["@int_field"].Value = i;
-					command.Parameters["@char_field"].Value = "IRow " + i.ToString();
-					command.Parameters["@varchar_field"].Value = "IRow Number " + i.ToString();
-					command.Parameters["@bigint_field"].Value = i;
-					command.Parameters["@smallint_field"].Value = i;
-					command.Parameters["@float_field"].Value = (float)(i + 10) / 5;
-					command.Parameters["@double_field"].Value = Math.Log(i, 10);
-					command.Parameters["@numeric_field"].Value = (decimal)(i + 10) / 5;
-					command.Parameters["@date_field"].Value = DateTime.Now;
-					command.Parameters["@time_field"].Value = DateTime.Now;
-					command.Parameters["@timestamp_field"].Value = DateTime.Now;
-					command.Parameters["@clob_field"].Value = "IRow Number " + i.ToString();
-					command.Parameters["@blob_field"].Value = Encoding.Default.GetBytes("IRow Number " + i.ToString());
-
-					command.ExecuteNonQuery();
-				}
-
-				// Commit transaction
-				transaction.Commit();
-			}
-			catch (FbException)
-			{
-				transaction.Rollback();
-				throw;
-			}
-			finally
-			{
-				command.Dispose();
-				transaction.Dispose();
-				connection.Close();
-			}
 		}
 	}
 }
