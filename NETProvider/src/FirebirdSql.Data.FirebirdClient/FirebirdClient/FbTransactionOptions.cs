ec71ad2847b92876b45506a430774ddba443e24d
Jiri Cincura <jiri@x2develop.com>
2015-08-23 18:01:24 +0200

    Refactoring.

--- NETProvider/src/FirebirdSql.Data.FirebirdClient/FirebirdClient/FbTransactionOptions.cs
+++ NETProvider/src/FirebirdSql.Data.FirebirdClient/FirebirdClient/FbTransactionOptions.cs
@@ -1,18 +1,18 @@
 /*
- *  Firebird ADO.NET Data provider for .NET and Mono 
- * 
- *     The contents of this file are subject to the Initial 
- *     Developer's Public License Version 1.0 (the "License"); 
- *     you may not use this file except in compliance with the 
- *     License. You may obtain a copy of the License at 
+ *  Firebird ADO.NET Data provider for .NET and Mono
+ *
+ *     The contents of this file are subject to the Initial
+ *     Developer's Public License Version 1.0 (the "License");
+ *     you may not use this file except in compliance with the
+ *     License. You may obtain a copy of the License at
  *     http://www.firebirdsql.org/index.php?op=doc&id=idpl
  *
- *     Software distributed under the License is distributed on 
- *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
- *     express or implied.  See the License for the specific 
+ *     Software distributed under the License is distributed on
+ *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *     express or implied.  See the License for the specific
  *     language governing rights and limitations under the License.
- * 
- *  Copyright (c) 2008-2010 Jiri Cincura (jiri@cincura.net)
+ *
+ *  Copyright (c) 2008-2010,2015 Jiri Cincura (jiri@cincura.net)
  *  All Rights Reserved.
  */
 
@@ -33,16 +33,13 @@ namespace FirebirdSql.Data.FirebirdClient
 				{
 					double secs = ((TimeSpan)value).TotalSeconds;
 					if (secs < 1 || secs > short.MaxValue)
-						throw new ArgumentException("The property value assigned is less than 1 or greater then short.MaxValue.");
+						throw new ArgumentException($"The value must be between 1 and {short.MaxValue}.");
 				}
 
 				_waitTimeout = value;
 			}
 		}
-		internal short? WaitTimeoutTPBValue
-		{
-			get { return _waitTimeout != null ? (short?)((TimeSpan)_waitTimeout).TotalSeconds : null; }
-		}
+		internal short? WaitTimeoutTPBValue => (short?)_waitTimeout?.TotalSeconds;
 
 		public FbTransactionBehavior TransactionBehavior { get; set; }
 
@@ -56,7 +53,7 @@ namespace FirebirdSql.Data.FirebirdClient
 			set
 			{
 				if (value == null)
-					throw new ArgumentNullException("LockTables cannot be null.");
+					throw new ArgumentNullException($"{nameof(LockTables)} cannot be null.");
 
 				_lockTables = value;
 			}
