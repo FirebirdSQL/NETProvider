40ec2fbfd11b6226a88dd69cc2308aafb219ffda
Jiri Cincura <jiri@x2develop.com>
2015-10-01 15:15:13 +0200

    Blank line

--- NETProvider/src/EntityFramework.Firebird/DefaultFbMigrationSqlGeneratorBehavior.cs
+++ NETProvider/src/EntityFramework.Firebird/DefaultFbMigrationSqlGeneratorBehavior.cs
@@ -1,20 +1,20 @@
 ﻿/*
- *	Firebird ADO.NET Data provider for .NET and Mono 
- * 
- *	   The contents of this file are subject to the Initial 
- *	   Developer's Public License Version 1.0 (the "License"); 
- *	   you may not use this file except in compliance with the 
- *	   License. You may obtain a copy of the License at 
+ *	Firebird ADO.NET Data provider for .NET and Mono
+ *
+ *	   The contents of this file are subject to the Initial
+ *	   Developer's Public License Version 1.0 (the "License");
+ *	   you may not use this file except in compliance with the
+ *	   License. You may obtain a copy of the License at
  *	   http://www.firebirdsql.org/index.php?op=doc&id=idpl
  *
- *	   Software distributed under the License is distributed on 
- *	   an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
- *	   express or implied. See the License for the specific 
+ *	   Software distributed under the License is distributed on
+ *	   an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *	   express or implied. See the License for the specific
  *	   language governing rights and limitations under the License.
- * 
+ *
  *	Copyright (c) 2014 Jiri Cincura (jiri@cincura.net)
  *	All Rights Reserved.
- *	
+ *
  */
 
 using System;
@@ -57,7 +57,6 @@ namespace FirebirdSql.Data.EntityFramework6
 
 			using (var writer = FbMigrationSqlGenerator.SqlWriter())
 			{
-
 				writer.Write("CREATE OR ALTER TRIGGER ");
 				writer.Write(FbMigrationSqlGenerator.Quote(CreateTriggerName(columnName, tableName)));
 				writer.Write(" ACTIVE BEFORE INSERT ON ");
