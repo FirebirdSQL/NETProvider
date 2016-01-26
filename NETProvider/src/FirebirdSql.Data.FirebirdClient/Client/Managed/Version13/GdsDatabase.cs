95e7b6ce761ad21b1ca152c96d1a0e7a9adf1911
Jiri Cincura <jiri@x2develop.com>
2016-01-26 11:33:20 +0100

    Header.

--- NETProvider/src/FirebirdSql.Data.FirebirdClient/Client/Managed/Version13/GdsDatabase.cs
+++ NETProvider/src/FirebirdSql.Data.FirebirdClient/Client/Managed/Version13/GdsDatabase.cs
@@ -1,20 +1,20 @@
 ﻿/*
- *	Firebird ADO.NET Data provider for .NET and Mono
+ *  Firebird ADO.NET Data provider for .NET and Mono
  *
- *	   The contents of this file are subject to the Initial
- *	   Developer's Public License Version 1.0 (the "License");
- *	   you may not use this file except in compliance with the
- *	   License. You may obtain a copy of the License at
- *	   http://www.firebirdsql.org/index.php?op=doc&id=idpl
+ *     The contents of this file are subject to the Initial
+ *     Developer's Public License Version 1.0 (the "License");
+ *     you may not use this file except in compliance with the
+ *     License. You may obtain a copy of the License at
+ *     http://www.firebirdsql.org/index.php?op=doc&id=idpl
  *
- *	   Software distributed under the License is distributed on
- *	   an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
- *	   express or implied. See the License for the specific
- *	   language governing rights and limitations under the License.
+ *     Software distributed under the License is distributed on
+ *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *     express or implied.  See the License for the specific
+ *     language governing rights and limitations under the License.
  *
- *	Copyright (c) 2009-2010 Jiri Cincura (jiri@cincura.net)
- *
- *	All Rights Reserved.
+ *  Copyright (c) 2016 Hajime Nakagami
+ *	Copyright (c) 2016 Jiri Cincura (jiri@cincura.net)
+ *  All Rights Reserved.
  */
 
 using System;
