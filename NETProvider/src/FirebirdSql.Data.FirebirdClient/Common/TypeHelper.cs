35b778aef64714817861b6143614196155ce11bb
Jiri Cincura <jiri@x2develop.com>
2016-03-07 18:32:06 +0100

    Simpler code.

--- NETProvider/src/FirebirdSql.Data.FirebirdClient/Common/TypeHelper.cs
+++ NETProvider/src/FirebirdSql.Data.FirebirdClient/Common/TypeHelper.cs
@@ -30,7 +30,7 @@ namespace FirebirdSql.Data.Common
 
 		public static bool IsDBNull(object value)
 		{
-			return (value == null || value == DBNull.Value);
+			return value == null || value == DBNull.Value;
 		}
 
 		public static short GetSize(DbDataType dataType)
