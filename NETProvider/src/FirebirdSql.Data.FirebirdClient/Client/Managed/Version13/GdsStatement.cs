0d881a1f434e74bd7a4a2215febed4447a8203a9
Jiri Cincura <jiri@x2develop.com>
2016-03-20 10:34:03 +0100

    Initial raw implementation of writing.

--- NETProvider/src/FirebirdSql.Data.FirebirdClient/Client/Managed/Version13/GdsStatement.cs
+++ NETProvider/src/FirebirdSql.Data.FirebirdClient/Client/Managed/Version13/GdsStatement.cs
@@ -44,7 +44,135 @@ namespace FirebirdSql.Data.Client.Managed.Version13
 
 		protected override byte[] WriteDescriptor(Descriptor descriptor, Charset charset)
 		{
-			return base.WriteDescriptor(descriptor, charset);
+			using (var xdr = new XdrStream(charset))
+			{
+				var bits = new BitArray(descriptor.Count);
+				for (int i = 0; i < descriptor.Count; i++)
+				{
+					var field = descriptor[i];
+					bits.Set(i, field.DbValue.IsDBNull());
+					bits.Set(i, field.DbDataType == DbDataType.Null);
+				}
+				var buffer = new byte[(int)Math.Ceiling(bits.Length / 8d)];
+				bits.CopyTo(buffer, 0);
+				xdr.WriteOpaque(buffer);
+
+				for (var i = 0; i < descriptor.Count; i++)
+				{
+					var field = descriptor[i];
+					if (field.DbValue.IsDBNull())
+					{
+						continue;
+					}
+					try
+					{
+						if (field.DbDataType != DbDataType.Null)
+						{
+							field.FixNull();
+
+							switch (field.DbDataType)
+							{
+								case DbDataType.Char:
+									if (field.Charset.IsOctetsCharset)
+									{
+										xdr.WriteOpaque(field.DbValue.GetBinary(), field.Length);
+									}
+									else
+									{
+										string svalue = field.DbValue.GetString();
+
+										if ((field.Length % field.Charset.BytesPerCharacter) == 0 &&
+											svalue.Length > field.CharCount)
+										{
+											throw IscException.ForErrorCodes(new[] { IscCodes.isc_arith_except, IscCodes.isc_string_truncation });
+										}
+
+										xdr.WriteOpaque(field.Charset.GetBytes(svalue), field.Length);
+									}
+									break;
+
+								case DbDataType.VarChar:
+									if (field.Charset.IsOctetsCharset)
+									{
+										xdr.WriteOpaque(field.DbValue.GetBinary(), field.Length);
+									}
+									else
+									{
+										string svalue = field.DbValue.GetString();
+
+										if ((field.Length % field.Charset.BytesPerCharacter) == 0 &&
+											svalue.Length > field.CharCount)
+										{
+											throw IscException.ForErrorCodes(new[] { IscCodes.isc_arith_except, IscCodes.isc_string_truncation });
+										}
+
+										byte[] data = field.Charset.GetBytes(svalue);
+
+										xdr.WriteBuffer(data, data.Length);
+									}
+									break;
+
+								case DbDataType.SmallInt:
+									xdr.Write(field.DbValue.GetInt16());
+									break;
+
+								case DbDataType.Integer:
+									xdr.Write(field.DbValue.GetInt32());
+									break;
+
+								case DbDataType.BigInt:
+								case DbDataType.Array:
+								case DbDataType.Binary:
+								case DbDataType.Text:
+									xdr.Write(field.DbValue.GetInt64());
+									break;
+
+								case DbDataType.Decimal:
+								case DbDataType.Numeric:
+									xdr.Write(field.DbValue.GetDecimal(), field.DataType, field.NumericScale);
+									break;
+
+								case DbDataType.Float:
+									xdr.Write(field.DbValue.GetFloat());
+									break;
+
+								case DbDataType.Guid:
+									xdr.WriteOpaque(field.DbValue.GetGuid().ToByteArray());
+									break;
+
+								case DbDataType.Double:
+									xdr.Write(field.DbValue.GetDouble());
+									break;
+
+								case DbDataType.Date:
+									xdr.Write(field.DbValue.GetDate());
+									break;
+
+								case DbDataType.Time:
+									xdr.Write(field.DbValue.GetTime());
+									break;
+
+								case DbDataType.TimeStamp:
+									xdr.Write(field.DbValue.GetDate());
+									xdr.Write(field.DbValue.GetTime());
+									break;
+
+								case DbDataType.Boolean:
+									xdr.Write(Convert.ToBoolean(field.Value));
+									break;
+
+								default:
+									throw IscException.ForStrParam($"Unknown SQL data type: {field.DataType}.");
+							}
+						}
+					}
+					catch (IOException ex)
+					{
+						throw IscException.ForErrorCode(IscCodes.isc_net_write_err, ex);
+					}
+				}
+				return xdr.ToArray();
+			}
 		}
 
 		#endregion
