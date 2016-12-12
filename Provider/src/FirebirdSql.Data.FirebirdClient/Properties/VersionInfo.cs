5a1fb456bea3e81318a653c741eaa975ce83f6b8
Jiri Cincura <jiri@x2develop.com>
2016-12-12 11:44:46 +0100

    Version number

--- Provider/src/FirebirdSql.Data.FirebirdClient/Properties/VersionInfo.cs
+++ Provider/src/FirebirdSql.Data.FirebirdClient/Properties/VersionInfo.cs
@@ -24,6 +24,6 @@ namespace FirebirdSql.Data.FirebirdClient.Properties
 {
 	static class VersionInfo
 	{
-		internal const string Version = "5.6.0.0";
+		internal const string Version = "5.7.0.0";
 	}
 }
