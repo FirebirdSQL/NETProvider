5631ed9e4c705f785cf87f1c57310ee368ea564b
Jiri Cincura <jiri@x2develop.com>
2017-01-10 11:39:43 +0100

    DNET-729

--- Provider/src/FirebirdSql.Data.FirebirdClient/Client/Managed/Compression/Zlib.cs
+++ Provider/src/FirebirdSql.Data.FirebirdClient/Client/Managed/Compression/Zlib.cs
@@ -100,7 +100,7 @@ namespace Ionic.Zlib
     /// <remarks>
     /// The different FlushType values are useful when using a Deflate in a streaming application.
     /// </remarks>
-    public enum FlushType
+    internal enum FlushType
     {
         /// <summary>No flush at all.</summary>
         None = 0,
@@ -140,7 +140,7 @@ namespace Ionic.Zlib
     /// <summary>
     /// The compression level to be used when using a DeflateStream or ZlibStream with CompressionMode.Compress.
     /// </summary>
-    public enum CompressionLevel
+    internal enum CompressionLevel
     {
         /// <summary>
         /// None means that the data will be simply stored, with no change at all.
@@ -219,7 +219,7 @@ namespace Ionic.Zlib
     /// work better on different sorts of data.  The strategy parameter can affect the compression
     /// ratio and the speed of compression but not the correctness of the compresssion.
     /// </summary>
-    public enum CompressionStrategy
+    internal enum CompressionStrategy
     {
         /// <summary>
         /// The default strategy is probably the best for normal data.
@@ -246,7 +246,7 @@ namespace Ionic.Zlib
     /// <summary>
     /// An enum to specify the direction of transcoding - whether to compress or decompress.
     /// </summary>
-    public enum CompressionMode
+    internal enum CompressionMode
     {
         /// <summary>
         /// Used to specify that the stream should compress the data.
@@ -263,7 +263,7 @@ namespace Ionic.Zlib
     /// A general purpose exception class for exceptions in the Zlib library.
     /// </summary>
     [Interop.GuidAttribute("ebc25cf6-9120-4283-b972-0e5520d0000E")]
-    public class ZlibException : System.Exception
+    internal class ZlibException : System.Exception
     {
         /// <summary>
         /// The ZlibException class captures exception information generated
@@ -465,7 +465,7 @@ namespace Ionic.Zlib
     /// </remarks>
     ///
     /// <exclude/>
-    public sealed class Adler
+    internal sealed class Adler
     {
         // largest prime smaller than 65536
         private static readonly uint BASE = 65521;
