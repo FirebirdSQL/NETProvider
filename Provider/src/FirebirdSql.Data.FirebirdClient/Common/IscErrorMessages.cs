e409fd4ec0db944e880a50bab12386132f5e5373
Jiri Cincura <jiri@x2develop.com>
2017-07-14 15:33:57 +0200

    Missing header.

--- Provider/src/FirebirdSql.Data.FirebirdClient/Common/IscErrorMessages.cs
+++ Provider/src/FirebirdSql.Data.FirebirdClient/Common/IscErrorMessages.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿/*
+ *	Firebird ADO.NET Data provider for .NET and Mono
+ *
+ *	   The contents of this file are subject to the Initial
+ *	   Developer's Public License Version 1.0 (the "License");
+ *	   you may not use this file except in compliance with the
+ *	   License. You may obtain a copy of the License at
+ *	   http://www.firebirdsql.org/index.php?op=doc&id=idpl
+ *
+ *	   Software distributed under the License is distributed on
+ *	   an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *	   express or implied. See the License for the specific
+ *	   language governing rights and limitations under the License.
+ *
+ *	Copyright (c) 2007-2017 Jiri Cincura (jiri@cincura.net)
+ *	All Rights Reserved.
+ */
+
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FirebirdSql.Data.Common
 {
-	static class IscErrorMessages
+	internal static class IscErrorMessages
 	{
 		static IDictionary<int, string> _messages = new Dictionary<int, string>()
 		{
