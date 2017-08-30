cf7e1f01ea17fda93efb20561e91b489bc19289b
Jiri Cincura <jiri@x2develop.com>
2017-08-30 15:31:11 +0200

    Added EFCore pack.

--- Provider/build.ps1
+++ Provider/build.ps1
@@ -39,6 +39,8 @@ function Pack() {
 	7z a -mx=9 $outDir\FirebirdSql.Data.FirebirdClient-$Version-netstandard2.0.7z $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\Release\netstandard2.0\FirebirdSql.Data.FirebirdClient.dll $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\Release\netstandard2.0\FirebirdSql.Data.FirebirdClient.pdb
 
 	7z a -mx=9 $outDir\EntityFramework.Firebird-$Version-net452.7z $baseDir\src\EntityFramework.Firebird\bin\Release\net452\EntityFramework.Firebird.dll $baseDir\src\EntityFramework.Firebird\bin\Release\net452\EntityFramework.Firebird.pdb
+
+	7z a -mx=9 $outDir\FirebirdSql.EntityFrameworkCore.Firebird-$Version-net452.7z $baseDir\src\FirebirdSql.EntityFrameworkCore.Firebird\bin\Release\netstandard2.0\FirebirdSql.EntityFrameworkCore.Firebird.dll $baseDir\src\FirebirdSql.EntityFrameworkCore.Firebird\bin\Release\netstandard2.0\FirebirdSql.EntityFrameworkCore.Firebird.pdb
 }
 
 function NuGet() {
