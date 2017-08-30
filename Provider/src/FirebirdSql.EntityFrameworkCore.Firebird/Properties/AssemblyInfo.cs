0764949fe14a6f0228612545e8f4e2ab6d64defa
Jiri Cincura <jiri@x2develop.com>
2017-08-30 14:08:20 +0200

    EFCore structure.

---
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *  Firebird ADO.NET Data provider for .NET and Mono
+ *
+ *     The contents of this file are subject to the Initial
+ *     Developer's Public License Version 1.0 (the "License");
+ *     you may not use this file except in compliance with the
+ *     License. You may obtain a copy of the License at
+ *     http://www.firebirdsql.org/index.php?op=doc&id=idpl
+ *
+ *     Software distributed under the License is distributed on
+ *     an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *     express or implied.  See the License for the specific
+ *     language governing rights and limitations under the License.
+ *
+ *  Copyright (c) 2014-2017 Jiri Cincura (jiri@cincura.net)
+ *  All Rights Reserved.
+ */
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("FirebirdSQL")]
+[assembly: AssemblyProduct("FirebirdClient")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyCopyright("(c) 2014-2017")]
+[assembly: AssemblyTitle("FirebirdClient - Entity Framework Core Provider")]
+[assembly: AssemblyDescription("FirebirdClient - Entity Framework Core Provider")]
+[assembly: AssemblyVersion(_VersionInfo.Version)]
+[assembly: AssemblyFileVersion(_VersionInfo.Version)]
