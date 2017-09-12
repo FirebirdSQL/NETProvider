986987353ffafb5ac7c539594c0c4cde9c356586
Jiri Cincura <jiri@x2develop.com>
2017-09-12 14:23:43 +0200

    Dummy code.

--- Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.Tests/Scratchpad.cs
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.Tests/Scratchpad.cs
@@ -36,6 +36,10 @@ namespace FirebirdSql.EntityFrameworkCore.Firebird.Tests
 						Test = x.Timestamp.Second,
 					})
 					.ToList();
+
+				db.Database.ExecuteSqlCommand("recreate table test (id int primary key, name varchar(20))");
+				db.Set<TestEntity>().Add(new TestEntity() { Id = 6, Name = "foobar" });
+				db.SaveChanges();
 			}
 		}
 	}
@@ -57,6 +61,11 @@ namespace FirebirdSql.EntityFrameworkCore.Firebird.Tests
 			monAttachmentConf.Property(x => x.AttachmentName).HasColumnName("MON$ATTACHMENT_NAME");
 			monAttachmentConf.Property(x => x.Timestamp).HasColumnName("MON$TIMESTAMP");
 			monAttachmentConf.ToTable("MON$ATTACHMENTS");
+
+			var testEntityConf = modelBuilder.Entity<TestEntity>();
+			testEntityConf.Property(x => x.Id).HasColumnName("ID");
+			testEntityConf.Property(x => x.Name).HasColumnName("NAME");
+			testEntityConf.ToTable("TEST");
 		}
 	}
 
@@ -65,5 +74,11 @@ namespace FirebirdSql.EntityFrameworkCore.Firebird.Tests
 		public int AttachmentId { get; set; }
 		public string AttachmentName { get; set; }
 		public DateTime Timestamp { get; set; }
+	}
+
+	class TestEntity
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
 	}
 }
