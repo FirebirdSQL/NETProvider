9a0630561a3b49f392a4cf5b5f2f62dd80db5328
Jiri Cincura <jiri@x2develop.com>
2017-09-13 08:53:09 +0200

    Refactoring.

---
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Reflection;
+using NUnitLite;
+
+namespace FirebirdSql.EntityFrameworkCore.Firebird.Tests
+{
+	static class Program
+	{
+		static int Main(string[] args)
+		{
+			args = args?.Any() ?? false
+				? args
+				: new[] { "--noresult" };
+			return new AutoRun(Assembly.GetExecutingAssembly()).Execute(args);
+		}
+	}
+}
