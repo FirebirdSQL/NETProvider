7428108bb3cbf47ea03196ba104746e19a77ece6
Jiri Cincura <jiri@x2develop.com>
2019-01-15 13:22:05 +0100

    Build script adjusted.

--- Provider/build.ps1
+++ Provider/build.ps1
@@ -1,18 +1,12 @@
 param(
-	[Parameter(Mandatory=$True)]$Configuration,
-	[Parameter(Mandatory=$True)]$Version)
+	[Parameter(Mandatory=$True)]$Configuration)
 
 $ErrorActionPreference = 'Stop'
 
 $baseDir = Split-Path -Parent $PSCommandPath
 $outDir = "$baseDir\out"
+$version = ''
 
-if ($env:build_nuget) {
-	$nuget = $env:build_nuget
-}
-else {
-	$nuget = 'I:\devel\bin\NuGet\nuget.exe'
-}
 if ($env:build_wix) {
 	$wix = $env:build_wix
 }
@@ -31,36 +25,35 @@ function Build() {
 	$solutionFile = "$baseDir\src\NETProvider.sln"
 	dotnet restore $solutionFile
 	dotnet msbuild /t:'Clean,Build' /p:Configuration=$Configuration $solutionFile /v:m /m
+	$script:version = (Get-Item $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\$Configuration\net452\FirebirdSql.Data.FirebirdClient.dll).VersionInfo.ProductVersion -replace '(\d+)\.(\d+)\.(\d+)(-[a-z0-9]+)?(.*)','$1.$2.$3$4'
 }
 
 function Pack() {
-	7z a -mx=9 $outDir\FirebirdSql.Data.FirebirdClient-$Version-net452.7z $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\Release\net452\FirebirdSql.Data.FirebirdClient.dll $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\Release\net452\FirebirdSql.Data.FirebirdClient.pdb | Out-Null
-	7z a -mx=9 $outDir\FirebirdSql.Data.FirebirdClient-$Version-netstandard1.6.7z $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\Release\netstandard1.6\FirebirdSql.Data.FirebirdClient.dll $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\Release\netstandard1.6\FirebirdSql.Data.FirebirdClient.pdb | Out-Null
-	7z a -mx=9 $outDir\FirebirdSql.Data.FirebirdClient-$Version-netstandard2.0.7z $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\Release\netstandard2.0\FirebirdSql.Data.FirebirdClient.dll $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\Release\netstandard2.0\FirebirdSql.Data.FirebirdClient.pdb | Out-Null
+	7z a -mx=9 $outDir\FirebirdSql.Data.FirebirdClient-$version-net452.7z $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\$Configuration\net452\FirebirdSql.Data.FirebirdClient.dll $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\$Configuration\net452\FirebirdSql.Data.FirebirdClient.pdb | Out-Null
+	7z a -mx=9 $outDir\FirebirdSql.Data.FirebirdClient-$version-netstandard1.6.7z $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\$Configuration\netstandard1.6\FirebirdSql.Data.FirebirdClient.dll $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\$Configuration\netstandard1.6\FirebirdSql.Data.FirebirdClient.pdb | Out-Null
+	7z a -mx=9 $outDir\FirebirdSql.Data.FirebirdClient-$version-netstandard2.0.7z $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\$Configuration\netstandard2.0\FirebirdSql.Data.FirebirdClient.dll $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\$Configuration\netstandard2.0\FirebirdSql.Data.FirebirdClient.pdb | Out-Null
 
-	7z a -mx=9 $outDir\EntityFramework.Firebird-$Version-net452.7z $baseDir\src\EntityFramework.Firebird\bin\Release\net452\EntityFramework.Firebird.dll $baseDir\src\EntityFramework.Firebird\bin\Release\net452\EntityFramework.Firebird.pdb | Out-Null
+	7z a -mx=9 $outDir\EntityFramework.Firebird-$version-net452.7z $baseDir\src\EntityFramework.Firebird\bin\$Configuration\net452\EntityFramework.Firebird.dll $baseDir\src\EntityFramework.Firebird\bin\$Configuration\net452\EntityFramework.Firebird.pdb | Out-Null
 
-	7z a -mx=9 $outDir\FirebirdSql.EntityFrameworkCore.Firebird-$Version-netstandard2.0.7z $baseDir\src\FirebirdSql.EntityFrameworkCore.Firebird\bin\Release\netstandard2.0\FirebirdSql.EntityFrameworkCore.Firebird.dll $baseDir\src\FirebirdSql.EntityFrameworkCore.Firebird\bin\Release\netstandard2.0\FirebirdSql.EntityFrameworkCore.Firebird.pdb | Out-Null
+	7z a -mx=9 $outDir\FirebirdSql.EntityFrameworkCore.Firebird-$version-netstandard2.0.7z $baseDir\src\FirebirdSql.EntityFrameworkCore.Firebird\bin\$Configuration\netstandard2.0\FirebirdSql.EntityFrameworkCore.Firebird.dll $baseDir\src\FirebirdSql.EntityFrameworkCore.Firebird\bin\$Configuration\netstandard2.0\FirebirdSql.EntityFrameworkCore.Firebird.pdb | Out-Null
 }
 
-function NuGet() {
-	& $nuget pack $baseDir\nuget\FirebirdSql.Data.FirebirdClient\FirebirdSql.Data.FirebirdClient.nuspec -OutputDirectory $outDir -Version $Version
-	& $nuget pack $baseDir\nuget\EntityFramework.Firebird\EntityFramework.Firebird.nuspec -OutputDirectory $outDir -Version $Version
-	& $nuget pack $baseDir\nuget\FirebirdSql.EntityFrameworkCore.Firebird\FirebirdSql.EntityFrameworkCore.Firebird.nuspec -OutputDirectory $outDir -Version $Version
+function NuGets() {
+	cp $baseDir\src\FirebirdSql.Data.FirebirdClient\bin\$Configuration\FirebirdSql.Data.FirebirdClient.$version.nupkg $outDir
+	cp $baseDir\src\EntityFramework.Firebird\bin\$Configuration\EntityFramework.Firebird.$version.nupkg $outDir
+	cp $baseDir\src\FirebirdSql.EntityFrameworkCore.Firebird\bin\$Configuration\FirebirdSql.EntityFrameworkCore.Firebird.$version.nupkg $outDir
 }
 
 function WiX() {
-	$wixVersion = $Version -replace '(.+?)(-.+)?','$1'
-	& $wix\candle.exe "-dBaseDir=$baseDir" "-dVersion=$wixVersion" -ext $wix\WixUtilExtension.dll -out $outDir\Installer.wixobj $baseDir\installer\Installer.wxs
-	& $wix\light.exe -ext $wix\WixUIExtension.dll -ext $wix\WixUtilExtension.dll -out $outDir\FirebirdSql.Data.FirebirdClient-$Version.msi $outDir\Installer.wixobj
+	$wixVersion = $version -replace '(.+?)(-[a-z0-9]+)?','$1'
+	& $wix\candle.exe "-dBaseDir=$baseDir" "-dVersion=$wixVersion" "-dConfiguration=$Configuration" -ext $wix\WixUtilExtension.dll -out $outDir\Installer.wixobj $baseDir\installer\Installer.wxs
+	& $wix\light.exe -ext $wix\WixUIExtension.dll -ext $wix\WixUtilExtension.dll -out $outDir\FirebirdSql.Data.FirebirdClient-$version.msi $outDir\Installer.wixobj
 	rm $outDir\Installer.wixobj
-	rm $outDir\FirebirdSql.Data.FirebirdClient-$Version.wixpdb
+	rm $outDir\FirebirdSql.Data.FirebirdClient-$version.wixpdb
 }
 
 Clean
 Build
-if ($Configuration -eq 'Release') {
-	Pack
-	NuGet
-	WiX
-}
+Pack
+NuGets
+WiX
