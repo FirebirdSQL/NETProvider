391b820ccc879888655e9a12fd2485f8ad0937b7
Jiri Cincura <jiri@x2develop.com>
2019-02-18 10:56:25 +0100

    Updated to EFCore 2.2 (DNET-869).

--- Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.Tests/Query/ToSqlExtension.cs
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.Tests/Query/ToSqlExtension.cs
@@ -15,41 +15,30 @@
 
 //$Authors = Jiri Cincura (jiri@cincura.net)
 
-using System;
 using System.Linq;
 using System.Reflection;
-using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
-using Remotion.Linq.Parsing.Structure;
 
 namespace FirebirdSql.EntityFrameworkCore.Firebird.Tests.Query
 {
-	// https://github.com/aspnet/EntityFrameworkCore/issues/6482
 	public static class ToSqlExtension
 	{
 		static readonly TypeInfo QueryCompilerTypeInfo = typeof(QueryCompiler).GetTypeInfo();
 		static readonly FieldInfo QueryCompilerField = typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.First(x => x.Name == "_queryCompiler");
-		static readonly PropertyInfo NodeTypeProviderField = QueryCompilerTypeInfo.DeclaredProperties.Single(x => x.Name == "NodeTypeProvider");
-		static readonly MethodInfo CreateQueryParserMethod = QueryCompilerTypeInfo.DeclaredMethods.First(x => x.Name == "CreateQueryParser");
-		static readonly FieldInfo DatabaseField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
+		static readonly FieldInfo QueryModelGeneratorField = QueryCompilerTypeInfo.DeclaredFields.First(x => x.Name == "_queryModelGenerator");
+		static readonly FieldInfo DataBaseField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
 		static readonly PropertyInfo DatabaseDependenciesField = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
 
 		public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
 		{
-			if (!(query is EntityQueryable<TEntity>) && !(query is InternalDbSet<TEntity>))
-			{
-				throw new ArgumentException("Invalid query");
-			}
-
-			var queryCompiler = (IQueryCompiler)QueryCompilerField.GetValue(query.Provider);
-			var nodeTypeProvider = (INodeTypeProvider)NodeTypeProviderField.GetValue(queryCompiler);
-			var parser = (IQueryParser)CreateQueryParserMethod.Invoke(queryCompiler, new object[] { nodeTypeProvider });
-			var queryModel = parser.GetParsedQuery(query.Expression);
-			var database = DatabaseField.GetValue(queryCompiler);
-			var queryCompilationContextFactory = ((DatabaseDependencies)DatabaseDependenciesField.GetValue(database)).QueryCompilationContextFactory;
-			var queryCompilationContext = queryCompilationContextFactory.Create(false);
+			var queryCompiler = (QueryCompiler)QueryCompilerField.GetValue(query.Provider);
+			var modelGenerator = (QueryModelGenerator)QueryModelGeneratorField.GetValue(queryCompiler);
+			var queryModel = modelGenerator.ParseQuery(query.Expression);
+			var database = (IDatabase)DataBaseField.GetValue(queryCompiler);
+			var databaseDependencies = (DatabaseDependencies)DatabaseDependenciesField.GetValue(database);
+			var queryCompilationContext = databaseDependencies.QueryCompilationContextFactory.Create(false);
 			var modelVisitor = (RelationalQueryModelVisitor)queryCompilationContext.CreateQueryModelVisitor();
 			modelVisitor.CreateQueryExecutor<TEntity>(queryModel);
 			var sql = modelVisitor.Queries.First().ToString();
