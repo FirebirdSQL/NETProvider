302b236d2acbac92f4d737c04203d671c84dfc2b
Jiri Cincura <jiri@x2develop.com>
2019-03-06 13:08:35 +0100

    More tests.

---
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.FunctionalTests/Query/AsyncSimpleQueryFbTest.cs
@@ -0,0 +1,37 @@
+﻿/*
+ *    The contents of this file are subject to the Initial
+ *    Developer's Public License Version 1.0 (the "License");
+ *    you may not use this file except in compliance with the
+ *    License. You may obtain a copy of the License at
+ *    https://github.com/FirebirdSQL/NETProvider/blob/master/license.txt.
+ *
+ *    Software distributed under the License is distributed on
+ *    an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *    express or implied. See the License for the specific
+ *    language governing rights and limitations under the License.
+ *
+ *    All Rights Reserved.
+ */
+
+//$Authors = Jiri Cincura (jiri@cincura.net)
+
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+
+namespace FirebirdSql.EntityFrameworkCore.Firebird.FunctionalTests.Query
+{
+	public class AsyncSimpleQueryFbTest : AsyncSimpleQueryTestBase<NorthwindQueryFbFixture<NoopModelCustomizer>>
+    {
+		public AsyncSimpleQueryFbTest(NorthwindQueryFbFixture<NoopModelCustomizer> fixture)
+			: base(fixture)
+		{ }
+
+		[Fact(Skip = "Missing view (and tables) for it.")]
+		public override Task Query_backed_by_database_view()
+		{
+			return base.Query_backed_by_database_view();
+		}
+	}
+}
---
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.FunctionalTests/Query/NullSemanticsQueryFbTest.cs
@@ -0,0 +1,43 @@
+/*
+ *    The contents of this file are subject to the Initial
+ *    Developer's Public License Version 1.0 (the "License");
+ *    you may not use this file except in compliance with the
+ *    License. You may obtain a copy of the License at
+ *    https://github.com/FirebirdSQL/NETProvider/blob/master/license.txt.
+ *
+ *    Software distributed under the License is distributed on
+ *    an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *    express or implied. See the License for the specific
+ *    language governing rights and limitations under the License.
+ *
+ *    All Rights Reserved.
+ */
+
+//$Authors = Jiri Cincura (jiri@cincura.net)
+
+using FirebirdSql.EntityFrameworkCore.Firebird.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestModels.NullSemanticsModel;
+
+namespace FirebirdSql.EntityFrameworkCore.Firebird.FunctionalTests.Query
+{
+	public class NullSemanticsQueryFbTest : NullSemanticsQueryTestBase<NullSemanticsQueryFbFixture>
+	{
+		public NullSemanticsQueryFbTest(NullSemanticsQueryFbFixture fixture)
+			: base(fixture)
+		{ }
+
+		protected override NullSemanticsContext CreateContext(bool useRelationalNulls = false)
+		{
+			var options = new DbContextOptionsBuilder(Fixture.CreateOptions());
+			if (useRelationalNulls)
+			{
+				new FbDbContextOptionsBuilder(options).UseRelationalNulls();
+			}
+			var context = new NullSemanticsContext(options.Options);
+			context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+			return context;
+		}
+	}
+}
