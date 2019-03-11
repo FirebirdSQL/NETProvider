a223e31de1061da4ea7017e135d1006969514313
Jiri Cincura <jiri@x2develop.com>
2019-03-11 13:49:33 +0100

    More tests

---
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.FunctionalTests/Query/ChangeTrackingFbTest.cs
@@ -0,0 +1,36 @@
+/*
+ *    The contents of this file are subject to the Initial
+ *    Developer's Public License Version 1.0 (the "License");
+ *    you may not use this file except in compliance with the
+ *    License. You may obtain a copy of the License at
+ *    https://github.com/FirebirdSQL/NETProvider/blob/master/license.txt.
+ *
+ *    Software distributed under the License is distributed on
+ *    an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ *    express or implied. See the License for the specific
+ *    language governing rights and limitations under the License.
+ *
+ *    All Rights Reserved.
+ */
+
+//$Authors = Jiri Cincura (jiri@cincura.net)
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestModels.Northwind;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+
+namespace FirebirdSql.EntityFrameworkCore.Firebird.FunctionalTests.Query
+{
+	public class ChangeTrackingFbTest : ChangeTrackingTestBase<NorthwindQueryFbFixture<NoopModelCustomizer>>
+	{
+		public ChangeTrackingFbTest(NorthwindQueryFbFixture<NoopModelCustomizer> fixture)
+			: base(fixture)
+		{ }
+
+		protected override NorthwindContext CreateNoTrackingContext()
+			=> new NorthwindRelationalContext(
+				new DbContextOptionsBuilder(Fixture.CreateOptions())
+					.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).Options);
+	}
+}
