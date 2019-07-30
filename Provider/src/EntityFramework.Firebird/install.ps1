c317aa3cd0102b0bbf6cfb6f090d8e526db26d3d
Jiri Cincura <jiri@x2develop.com>
2019-07-30 11:20:40 +0200

    Using XDT for transformations.
    * DNET-883
    * https://github.com/aspnet/EntityFramework6/pull/953

--- Provider/src/EntityFramework.Firebird/install.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package, $project)                                                                                                                    
-Add-EFProvider $project 'FirebirdSql.Data.FirebirdClient' 'EntityFramework.Firebird.FbProviderServices, EntityFramework.Firebird'
-Add-EFDefaultConnectionFactory $project 'EntityFramework.Firebird.FbConnectionFactory, EntityFramework.Firebird'
