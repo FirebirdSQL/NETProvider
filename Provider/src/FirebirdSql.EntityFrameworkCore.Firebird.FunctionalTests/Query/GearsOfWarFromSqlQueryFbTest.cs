a2bd60a334078fe989b7e47e5256b6a6c811e4f6
Jiri Cincura <jiri@x2develop.com>
2021-11-01 08:49:25 +0100

    Better license link.

--- Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.FunctionalTests/Query/GearsOfWarFromSqlQueryFbTest.cs
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird.FunctionalTests/Query/GearsOfWarFromSqlQueryFbTest.cs
@@ -1,9 +1,9 @@
-/*
+﻿/*
  *    The contents of this file are subject to the Initial
  *    Developer's Public License Version 1.0 (the "License");
  *    you may not use this file except in compliance with the
  *    License. You may obtain a copy of the License at
- *    https://github.com/FirebirdSQL/NETProvider/blob/master/license.txt.
+ *    https://github.com/FirebirdSQL/NETProvider/raw/master/license.txt.
  *
  *    Software distributed under the License is distributed on
  *    an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
--- Provider/src/FirebirdSql.EntityFrameworkCore.Firebird/Extensions/FbDbContextOptionsBuilderExtensions.cs
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird/Extensions/FbDbContextOptionsBuilderExtensions.cs
@@ -3,7 +3,7 @@
  *    Developer's Public License Version 1.0 (the "License");
  *    you may not use this file except in compliance with the
  *    License. You may obtain a copy of the License at
- *    https://github.com/FirebirdSQL/NETProvider/blob/master/license.txt.
+ *    https://github.com/FirebirdSQL/NETProvider/raw/master/license.txt.
  *
  *    Software distributed under the License is distributed on
  *    an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
