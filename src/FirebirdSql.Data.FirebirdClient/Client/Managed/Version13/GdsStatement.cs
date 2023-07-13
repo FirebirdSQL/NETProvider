651f5bcf75bf023caea2d37a2153e80434871bf5
BFuerchau <92720745+BFuerchau@users.noreply.github.com>
2023-07-13 17:36:49 +0200

    Update GdsStatement.cs  Version 13
    matching override to gdsstatement version 10

--- src/FirebirdSql.Data.FirebirdClient/Client/Managed/Version13/GdsStatement.cs
+++ src/FirebirdSql.Data.FirebirdClient/Client/Managed/Version13/GdsStatement.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  *    The contents of this file are subject to the Initial
  *    Developer's Public License Version 1.0 (the "License");
  *    you may not use this file except in compliance with the
@@ -131,6 +131,34 @@ internal class GdsStatement : Version12.GdsStatement
 		}
 	}
 
+	protected override object[] ReadRowObjectValues()
+	{
+		var row = new object[_fields.Count];
+		try
+		{
+			if (_fields.Count > 0)
+			{
+				var nullBytes = _database.Xdr.ReadOpaque((int)Math.Ceiling(_fields.Count / 8d));
+				var nullBits = new BitArray(nullBytes);
+				for (var i = 0; i < _fields.Count; i++)
+				{
+					if (nullBits.Get(i))
+					{
+						row[i] = DBNull.Value;
+					}
+					else
+					{
+						row[i] = ReadRawValue(_database.Xdr, _fields[i]);
+					}
+				}
+			}
+		}
+		catch (IOException ex)
+		{
+			throw IscException.ForIOException(ex);
+		}
+		return row;
+	}
 	protected override DbValue[] ReadRow()
 	{
 		var row = new DbValue[_fields.Count];
@@ -179,6 +207,34 @@ internal class GdsStatement : Version12.GdsStatement
 					{
 						var value = await ReadRawValueAsync(_database.Xdr, _fields[i], cancellationToken).ConfigureAwait(false);
 						row[i] = new DbValue(this, _fields[i], value);
+					}
+				}
+			}
+		}
+		catch (IOException ex)
+		{
+			throw IscException.ForIOException(ex);
+		}
+		return row;
+	}
+	protected override async ValueTask<object[]> ReadRowObjectValuesAsync(CancellationToken cancellationToken = default)
+	{
+		var row = new object[_fields.Count];
+		try
+		{
+			if (_fields.Count > 0)
+			{
+				var nullBytes = await _database.Xdr.ReadOpaqueAsync((int)Math.Ceiling(_fields.Count / 8d), cancellationToken).ConfigureAwait(false);
+				var nullBits = new BitArray(nullBytes);
+				for (var i = 0; i < _fields.Count; i++)
+				{
+					if (nullBits.Get(i))
+					{
+						row[i] = DBNull.Value;
+					}
+					else
+					{
+						row[i] = await ReadRawValueAsync(_database.Xdr, _fields[i], cancellationToken).ConfigureAwait(false);
 					}
 				}
 			}
