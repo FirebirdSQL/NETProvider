66b3b5bc19550f28aa52baf4510f554ca7e70da9
Mark Junker <m.junker@fubar-dev.de>
2023-12-04 18:18:10 +0100

    Use a ConcurrentDictionary to remember which calls did work
    This is needed to avoid unnecessary EntryPointNotFoundException exceptions, as they massively slow down program execution - especially in development environments.

--- src/FirebirdSql.Data.FirebirdClient/Client/Native/FbClientFactory.cs
+++ src/FirebirdSql.Data.FirebirdClient/Client/Native/FbClientFactory.cs
@@ -83,7 +83,8 @@ internal static class FbClientFactory
 				{
 					result = BuildFbClient(dllName);
 					cache.Add(dllName, result);
-					ShutdownHelper.RegisterFbClientShutdown(() => NativeHelpers.CallIfExists(() => result.fb_shutdown(0, 0)));
+					ShutdownHelper.RegisterFbClientShutdown(
+						() => NativeHelpers.CallIfExists("shutdown", () => result.fb_shutdown(0, 0)));
 					return result;
 				}
 				finally
