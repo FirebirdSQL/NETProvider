6f9e2d8e7f4fb51cdce1ae4a877e2e7ab65858cd
F.D.Castel <fdcastel@gmail.com>
2026-03-22 23:41:00 -0300

    Add -Disasm and -Profile flags to run-benchmark.ps1

--- run-benchmark.ps1
+++ run-benchmark.ps1
@@ -1,15 +1,23 @@
 param(
-	[ValidateSet('CommandBenchmark','LargeFetchBenchmark')]
-	$Benchmark = 'CommandBenchmark'
+	[ValidateSet('CommandBenchmark','ConnectionBenchmark','LargeFetchBenchmark')]
+	$Benchmark = 'CommandBenchmark',
+
+	[switch]$Disasm,
+	[switch]$Profile
 )
 
 $ErrorActionPreference = 'Stop'
 
 $projectFile = '.\src\FirebirdSql.Data.FirebirdClient.Benchmarks\FirebirdSql.Data.FirebirdClient.Benchmarks.csproj'
 
+$extraArgs = @()
+if ($Disasm) { $extraArgs += '--disasm' }
+if ($Profile) { $extraArgs += '--profiler', 'ETW' }
+
 # Run selected benchmark
 dotnet run `
     --project $projectFile `
     --configuration 'Release' `
     -- `
-    --filter "*$($Benchmark)*"
+    --filter "*$($Benchmark)*" `
+    @extraArgs
